dec1db630c49643ab10bf2c567f4b9fb497ade09
bgtripp <bentrippx@gmail.com>
2024-10-27 23:51:18 -0700

    Scrape supplemental data and save json

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from bs4 import BeautifulSoup
 import weasyprint
 from dotenv import load_dotenv
@@ -15,10 +16,10 @@ load_dotenv()
 #   "style": "Post-Impressionism"
 # }
 
-# Helper function to scrape data for each painting
+# Helper function to scrape URIs and grab images for each painting
 def scrape_paintings(image_dir):
     list_url = "https://www.wikiart.org/en/api/2/MostViewedPaintings"
-    painting_uris = []
+    painting_data = {}
     has_more = True
 
     session_key = get_wikiart_session()
@@ -39,12 +40,16 @@ def scrape_paintings(image_dir):
                 pagination_token = jsonResponse["paginationToken"]
                 
                 for painting in jsonResponse["data"]:
-                    painting_uris.append(os.path.join(painting["artistUrl"], painting["url"]))
+                    painting_data[os.path.join(painting["artistUrl"], painting["url"])] = {
+                        "artist" : painting["artistName"],
+                        "title_of_work" : painting["title"],
+                        "date_created" : painting["completitionYear"] # No, this is not a typo
+                    }
                     save_painting_image(painting, image_dir)
 
         except Exception as e:
             print(f"Error fetching page URLs: {e}")
-    return painting_uris
+    return painting_data
 
 # Generates API session token
 def get_wikiart_session():
@@ -78,16 +83,37 @@ def save_page_as_pdf(uri, output_dir):
     except Exception as e:
         print(f"Error saving PDF for {url}: {e}")
 
+# Scrape additional data directly from page and write to JSON
+def write_data_from_page(uri, painting_data, json_dir):
+    base_url = "https://www.wikiart.org/en/"
+    response = requests.get(base_url + uri)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for li in soup.find_all('li'):
+            if li.find('s') and "Style" in li.find('s').text:
+                painting_data[uri]["style"] = li.find('a').text.strip()
+            if li.find('s') and "Location" in li.find('s').text:
+                painting_data[uri]["location"] = li.find('span').text.strip()
+
+    save_path = os.path.join(json_dir, uri.replace("/", "_") + ".json")
+    with open(save_path, 'w') as f:
+        json.dump(painting_data[uri], f, indent=4)
+    print("Saved json: " + save_path)
+
 # Main scraper function
 def scrape_wikiart(output_dir):
     # Get all page URLs from the website
     image_dir = os.path.join(output_dir, "images")
-    page_urls = scrape_paintings(image_dir)
+    painting_data = scrape_paintings(image_dir)
+
+    pdf_dir = os.path.join(output_dir, "pdfs")
+    json_dir = os.path.join(output_dir, "json")
     
     # Iterate over each URL and save as PDF
-    pdf_dir = os.path.join(output_dir, "pdfs")
-    for url in page_urls:
-        save_page_as_pdf(url, pdf_dir)
+    for uri in painting_data.keys():
+        write_data_from_page(uri, painting_data, json_dir)
+        save_page_as_pdf(uri, pdf_dir)
 
 if __name__ == "__main__":
     output_dir = "./data"
